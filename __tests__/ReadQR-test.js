95a7844b2c02d1c6263aeb55588adcfae8ccc113
Sofia <sofiabeleninchausti@gmail.com>
2021-10-19 16:42:28 -0300

    delete imports without use

--- __tests__/ReadQR-test.js
+++ __tests__/ReadQR-test.js
@@ -1,18 +1,16 @@
 import 'react-native'
 import React from 'react';
-import {create,act} from 'react-test-renderer'
 import { createStore } from 'redux';
 import {Provider} from 'react-redux'
 import reducer from '../src/reducer/index'
 import {render} from "@testing-library/react-native"
 import ReadQR from "../screens/ReadQR"
 
-import { mount, shallow } from 'enzyme'
-import configureMockStore from 'redux-mock-store'
 
 const store=createStore(reducer,{status:'default'})
 
 let component;
+
 describe ("<ReadQR/>",()=>{
     beforeEach(()=>{
         component=render(<Provider store={store}><ReadQR/></Provider>)
