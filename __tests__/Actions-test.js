51d15b997b6d378e03f78bd726ad224decbcd772
Sofia <sofiabeleninchausti@gmail.com>
2021-10-19 09:49:47 -0300

    codigo structure

--- __tests__/Actions-test.js
+++ __tests__/Actions-test.js
@@ -1,19 +1,24 @@
-import {addQR,filterSearch,removeItem} from "../src/actions/index"
+import {
+  addQR,
+  filterSearch,
+  removeItem
+} from "../src/actions/index"
 
 it('It should return an action with the properties type "ADD_QR" and a payload, its value is received by argument', () => {
-    expect(addQR('text')).toEqual({
-        type:'ADD_QR',payload:'text'
-    })
+  expect(addQR('text')).toEqual({
+    type: 'ADD_QR',
+    payload: 'text'
   })
+})
 it('It should return an action with the properties type "FILTER" and a payload, its value is received by argument', () => {
-    expect(filterSearch('text')).toEqual({
-        type:'FILTER',payload:'text'
-    })
+  expect(filterSearch('text')).toEqual({
+    type: 'FILTER',
+    payload: 'text'
   })
-  it('It should return an action with the properties type "REMOVE" and a payload, its value is received by argument', () => {
-    expect(removeItem('text')).toEqual({
-        type:'REMOVE',payload:'text'
-    })
+})
+it('It should return an action with the properties type "REMOVE" and a payload, its value is received by argument', () => {
+  expect(removeItem('text')).toEqual({
+    type: 'REMOVE',
+    payload: 'text'
   })
-
-
+})
